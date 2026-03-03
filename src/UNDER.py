b7b79c1bd51fefdf62393246ff280de65314e777
shash <shashwat.srma@gmail.com>
2026-03-03 15:54:02 +0545

    report on unique combined dataset performed pretty good i think this is the farmost unbiased and good version of fabricated dataset we have

--- src/UNDER.py
+++ src/UNDER.py
@@ -24,7 +24,7 @@ print("Using device:", device)
 
 
 # 3. LOAD DATASET
-df = pd.read_csv("data/coV7+V8.csv", encoding="latin1")
+df = pd.read_csv("data/coV7+V8uniqf.csv", encoding="latin1")
 
 df["content"] = df["TITLE"]
 df["Label"] = df["LABEL"].map({"TRUE": 0, "Fake": 1})  # 0 = Real, 1 = Fake
@@ -60,7 +60,7 @@ df["content"] = df["content"].apply(clean_text)
 # 5. STRATIFIED SAMPLING (LIMIT TO 3000)
 df_limited, _ = train_test_split(
     df,
-    train_size=67000,
+    train_size=47069,
     stratify=df["Label"],
     random_state=42
 )
